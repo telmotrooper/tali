f77ed9aa0c829139c11def50f016312fef83f63a
Telmo Henrique Valverde <telmo.trooper@gmail.com>
2019-03-14 18:59:13 -0300

    Ask for network type

--- install.py
+++ install.py
@@ -7,3 +7,14 @@ separator = "-" * int(columns)
 
 print("Telmo's Arch Linux Installer")
 print(separator)
+
+while True:   # do while
+  network = input("Are you using Wi-Fi or a wired network? ")
+
+  if(network.lower() == "wifi" or network.lower() == "wi-fi"):
+    print("Wi-Fi")
+    break
+  elif(network.lower() == "wired" or network.lower() == "ethernet"):
+    print("Wired")
+    break
+
