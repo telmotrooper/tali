f2ea04232f4971a56e51f51d89682bbb37d8db46
Telmo Henrique Valverde <telmo.trooper@gmail.com>
2019-03-14 20:14:48 -0300

    Using 'check_output' instead of 'popen'

--- install.py
+++ install.py
@@ -2,22 +2,13 @@
 
 import os
 import re
+import subprocess
 
 rows, columns = os.popen('stty size', 'r').read().split()
 separator = "-" * int(columns)
 
 print("Telmo's Arch Linux Installer")
 print(separator)
-
-# while True:   # do while
-#   network = input("Are you using Wi-Fi or a wired network? ")
-
-#   if(network.lower() == "wifi" or network.lower() == "wi-fi"):
-#     print("Wi-Fi")
-#     break
-#   elif(network.lower() == "wired" or network.lower() == "ethernet"):
-#     print("Wired")
-#     break
 
 text = """phy#0
     Interface wlp6s0
@@ -40,9 +31,9 @@ if(match != None):
   network_interface = (match.group(0).split(" ")[1])
   print("Wi-Fi interface: {}".format(network_interface))
 else:
-  print("No Wi-Fi interface found")
+  print("No Wi-Fi interface found.")
 
-boot = os.popen('ls /sys/firmware/efi/efivars', 'r').read()
+boot = subprocess.check_output(['ls', '/sys/firmware/efi/efivars'])
 if(boot == ''):
   boot = "BIOS"
 else:
