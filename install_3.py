8c5472bceba1bea63d3bbab060d29f3a9ad7fd83
Telmo "Trooper <telmo.trooper@gmail.com>
2019-06-13 23:06:11 -0300

    Added more functions to part 3

--- install_3.py
+++ install_3.py
@@ -4,3 +4,5 @@ import os
 
 os.system("ln -sf /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime")
 os.system("hwclock --systohc")
+os.system("cat /etc/locale.gen | sed 's/#en_US.UTF-8/en_US.UTF-8/g' > /etc/locale.gen")
+os.system("locale-gen")
