7b3a53c833df3d8f27701cea0a25ea8af1c046cf
Telmo "Trooper <telmo.trooper@gmail.com>
2019-06-14 00:03:16 -0300

    More stuff done on part 3

--- install_3.py
+++ install_3.py
@@ -7,3 +7,8 @@ os.system("hwclock --systohc")
 os.system("cat /etc/locale.gen | sed 's/#en_US.UTF-8/en_US.UTF-8/g' > /locale.gen")
 os.system("mv /locale.gen /etc/")
 os.system("locale-gen")
+os.system("echo 'LANG=en_US.UTF-8' > /etc/locale.conf")
+os.system("echo 'KEYMAP=br-abnt2' > /etc/vconsole.conf")
+
+hostname = input("What should be this computer's name?")
+os.system("echo '{}' > /etc/hostname".format(hostname))
