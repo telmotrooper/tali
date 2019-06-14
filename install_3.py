7ebadcfdfc24114acf9aeda72852c982b6947bce
Telmo "Trooper <telmo.trooper@gmail.com>
2019-06-13 23:53:00 -0300

    Workaround for locale.gen bug

--- install_3.py
+++ install_3.py
@@ -4,5 +4,6 @@ import os
 
 os.system("ln -sf /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime")
 os.system("hwclock --systohc")
-os.system("cat /etc/locale.gen | sed 's/#en_US.UTF-8/en_US.UTF-8/g' > /etc/locale.gen")
+os.system("cat /etc/locale.gen | sed 's/#en_US.UTF-8/en_US.UTF-8/g' > /locale.gen")
+os.system("mv /locale.gen /etc/")
 os.system("locale-gen")
