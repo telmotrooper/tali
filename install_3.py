7d912de69a2f78c5c38b5e8d5d17eef261a1aee9
Telmo "Trooper <telmo.trooper@gmail.com>
2019-06-17 20:08:45 -0300

    GRUB should install now

--- install_3.py
+++ install_3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import os
+import os, subprocess
 
 os.system("ln -sf /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime")
 os.system("hwclock --systohc")
@@ -12,7 +12,9 @@ os.system("echo 'KEYMAP=br-abnt2' > /etc/vconsole.conf")
 
 hostname = input("Set your computer's name: ")
 os.system("echo '{}' > /etc/hostname".format(hostname))
-os.system("pacman -S --noconfirm ttf-bitstream-vera grub gdm cinnamon gnome-terminal firefox gnome-system-monitor")
+os.system("pacman -S --noconfirm ttf-bitstream-vera grub "
+  "gdm cinnamon gnome-terminal firefox gnome-system-monitor"
+  "arc-gtk-theme papirus-icon-theme")
 os.system("systemctl enable gdm")
 os.system("systemctl enable NetworkManager")
 print("Set the root password: ")
@@ -24,3 +26,22 @@ os.system("passwd {}".format(username))
 os.system("cat /etc/sudoers | sed 's/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/g' > /etc/sudoers_new")
 os.system("mv /etc/sudoers_new /etc/sudoers")
 
+ls_efi = subprocess.check_output(
+  "ls /sys/firmware/efi/efivars; exit 0;",
+  shell=True, stderr=subprocess.STDOUT).decode()
+
+if(ls_efi[:2] == "ls"):
+  boot = "BIOS"
+else:
+  boot = "UEFI"
+
+if(boot == "UEFI"):
+  os.system("pacman -S --noconfirm efibootmgr")
+  os.system("grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB")
+else:
+  print("The disks available are: ")
+  os.system("fdisk -l | grep 'Disk /' | awk '{print $2, $3, $4}'")
+  disk = input("In which disk should GRUB be installed? ")
+  os.system("grub-install --target=i386-pc {}".format(disk))
+
+os.system("grub-mkconfig -o /boot/grub/grub.cfg")
