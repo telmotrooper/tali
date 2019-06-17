cb4dca0daddb260f48e28be76d6554b41f7828cc
Telmo "Trooper <telmo.trooper@gmail.com>
2019-06-17 19:28:17 -0300

    NetworkManager enabled by default

--- install_3.py
+++ install_3.py
@@ -12,8 +12,9 @@ os.system("echo 'KEYMAP=br-abnt2' > /etc/vconsole.conf")
 
 hostname = input("Set your computer's name: ")
 os.system("echo '{}' > /etc/hostname".format(hostname))
-os.system("pacman -S ttf-bitstream-vera gdm cinnamon")
+os.system("pacman -S --noconfirm ttf-bitstream-vera grub gdm cinnamon gnome-terminal firefox gnome-system-monitor")
 os.system("systemctl enable gdm")
+os.system("systemctl enable NetworkManager")
 print("Set the root password: ")
 os.system("passwd")
 username = input("Set your username: ")
@@ -23,4 +24,3 @@ os.system("passwd {}".format(username))
 os.system("cat /etc/sudoers | sed 's/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/g' > /etc/sudoers_new")
 os.system("mv /etc/sudoers_new /etc/sudoers")
 
-# gotta set root password yet
