68f1e3d0d03ac8a4822534bbc603e2843b962e1b
Telmo "Trooper <telmo.trooper@gmail.com>
2019-07-23 19:49:37 -0300

    Added more prints and added single point to write password

--- install_3.py
+++ install_3.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python3
 
 import os, subprocess
+from getpass import getpass
 
 os.system("pacman -S --noconfirm ttf-bitstream-vera grub "
   "gdm cinnamon gnome-terminal firefox gnome-system-monitor "
   "arc-gtk-theme papirus-icon-theme zsh git go ttf-droid xorg-xkill neofetch")
 
+print("Enabling the display manager")
 os.system("systemctl enable gdm")
 os.system("systemctl enable NetworkManager")
 
+print("Synchroning the clock")
 os.system("ln -sf /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime")
 os.system("hwclock --systohc")
+
+print("Setting up locale")
 os.system("cat /etc/locale.gen | sed 's/#en_US.UTF-8/en_US.UTF-8/g' > /locale.gen")
 os.system("mv /locale.gen /etc/")
 os.system("locale-gen")
@@ -20,24 +25,30 @@ os.system("echo 'KEYMAP=br-abnt2' > /etc/vconsole.conf")
 hostname = input("Set your computer's name: ")
 os.system("echo '{}' > /etc/hostname".format(hostname))
 
-print("Set the root password")
-os.system("passwd")
 username = input("Set your username: ")
 os.system("useradd -m -G wheel -s /bin/zsh {}".format(username))
-print("Set the password for {}".format(username))
-os.system("passwd {}".format(username))
+
+password1 = "1"
+password2 = "2"
+
+while(password1 != password2):
+  password1 = getpass("Set your password: ")
+  password2 = getpass("Repeat your password: ")
+  
+  if password1 != password2:
+    print("The passwords don't match.")
+
+os.system(f"passwd root {password1}")
+os.system(f"passwd {username} {password1}")
 os.system("cat /etc/sudoers | sed 's/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/g' > /etc/sudoers_new")
 os.system("mv /etc/sudoers_new /etc/sudoers")
 
-# Install "oh-my-zsh"
+print("Installing oh-my-zsh and its plugins")
 os.system("sudo -u {} sh -c \"$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh) --unattended\"".format(username))
-
-# Install "zsh-autosuggestions"
 os.system("sudo -u {} sh -c \"git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions\"".format(username))
-
-# Install "zsh-syntax-highlighting"
 os.system("sudo -u {} sh -c \"git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting\"".format(username))
 
+print("Installing yay")
 # Build "yay" as user, since "makepkg" cannot be executed as "sudo"
 os.system("sudo -u {} sh -c \"cd ~ && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -s\"".format(username))
 
