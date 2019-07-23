545dfea7c15c6ee181939ccb298af7b115cf2c8d
Telmo "Trooper <telmo.trooper@gmail.com>
2019-07-23 18:33:03 -0300

    Attempt at making a swap partition

--- quick_partitioning.py
+++ quick_partitioning.py
@@ -1,15 +1,31 @@
 #!/usr/bin/env python3
 
-import os
+import os, subprocess
+
+ram = subprocess.check_output(
+  "free -m | grep Mem | awk '{print $2}'",
+  shell=True, stderr=subprocess.STDOUT).decode().rstrip()
+
+print("We detected {} MiB of RAM, a swap partition is gonna be created with this size.".format(ram))
+
+swap_end = 101 + int(ram)
 
 print("The disks available are: ")
 os.system("fdisk -l | grep 'Disk /' | awk '{print $2, $3, $4}'")
 disk = input("Which disk should be partitioned? ")
-os.system("parted --script {} \ mklabel msdos \ mkpart primary 1MiB 101MiB \ set 1 boot on \ mkpart primary 101MiB 100%".format(disk))
+os.system(f
+  "parted --script {disk} \ " +
+  "mklabel msdos \ " +
+  "mkpart primary 1MiB 101MiB \ " +
+  "set 1 boot on \ " +
+  "mkpart primary linux-swap 101MiB {swap_end} \ " +
+  "mkpart primary 101MiB 100%")
 
 print("--- Formatting partitions ---")
 os.system("mkfs.ext4 /dev/sda1")
-os.system("mkfs.ext4 /dev/sda2")
+os.system("mkswap /dev/sda2")
+os.system("swapon /dev/sda2")
+os.system("mkfs.ext4 /dev/sda3")
 
 print("--- Mounting partitions ---")
 os.system("mount /dev/sda2 /mnt")
