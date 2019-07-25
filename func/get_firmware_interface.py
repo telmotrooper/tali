7632241299a2613138fa79a0ff25ea991a4a79dc
Telmo "Trooper <telmo.trooper@gmail.com>
2019-07-25 18:38:17 -0300

    Changed 'ls_efi' to 'ls_efi_vars', which is clearer

--- func/get_firmware_interface.py
+++ func/get_firmware_interface.py
@@ -1,14 +1,14 @@
 import subprocess
 
 def get_firmware_interface():
-  ls_efi = subprocess.check_output(
+  ls_efi_vars = subprocess.check_output(
     "ls /sys/firmware/efi/efivars; exit 0;",
     shell=True,
     stderr=subprocess.STDOUT).decode()
 
   # The output only starts with "ls" if the command failed, which
   # is what happens when your system does not have UEFI variables.
-  if(ls_efi[:2] == "ls"):
+  if(ls_efi_vars[:2] == "ls"):
     return "BIOS"
   else:
     return "UEFI"
