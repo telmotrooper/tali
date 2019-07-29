14cae1644e5fc1d1b19164dffae14b385fd56136
Telmo "Trooper <telmo.trooper@gmail.com>
2019-07-29 19:10:48 -0300

    Should be able to partition both UEFI and BIOS

--- quick_partitioning.py
+++ quick_partitioning.py
@@ -8,22 +8,39 @@ ram = get_ram_amount()
 
 print(f"We detected {ram} MiB of RAM, a swap partition is gonna be created with this size.".format(ram))
 
-swap_end = 101 + int(ram)
-
+swap_end_bios = 101 + int(ram)
+swap_end_uefi = 261 + int(ram)
 
 print("Which disk should be partitioned? ")
 disk = select_disk()
 
-os.system(f"""
-  parted --script {disk} \
-  mklabel msdos \
-  mkpart primary 1MiB 101MiB \
-  set 1 boot on \
-  mkpart primary linux-swap 101MiB {swap_end}MiB \
-  mkpart primary {swap_end}MiB 100%""")
+fw_interface = get_firmware_interface()
+
+if(fw_interface == "BIOS"):
+  os.system(f"""
+    parted --script {disk} \
+    mklabel msdos \
+    mkpart primary ext4 1MiB 101MiB \
+    set 1 boot on \
+    mkpart primary linux-swap 101MiB {swap_end_bios}MiB \
+    mkpart primary ext4 {swap_end_bios}MiB 100%""")
+
+else: # UEFI
+  os.system(f"""
+    parted --script {disk} \
+    mklabel gpt \
+    mkpart primary fat32 1MiB 261MiB \
+    set 1 esp on \
+    mkpart primary linux-swap 261MiB {swap_end_uefi}MiB \
+    mkpart primary ext4 {swap_end_uefi}MiB 100%""")
+
 
 print("--- Formatting partitions ---")
-os.system("mkfs.ext4 /dev/sda1")
+if(fw_interface == "BIOS"):
+  os.system("mkfs.ext4 /dev/sda1")
+else: # UEFI
+  os.system("mkfs.fat -F32 /dev/sda1")
+
 os.system("mkswap /dev/sda2")
 os.system("swapon /dev/sda2")
 os.system("mkfs.ext4 /dev/sda3")
