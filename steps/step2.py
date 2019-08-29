99afa9add6f2e765d48179fc23a6673a14294e71
Telmo "Trooper <telmo.trooper@gmail.com>
2019-08-29 20:28:34 -0300

    Using absolute path in step 2

--- steps/step2.py
+++ steps/step2.py
@@ -5,6 +5,6 @@ import os
 os.system("pacstrap /mnt base base-devel net-tools xdg-user-dirs")
 os.system("pacstrap /mnt python wget")
 os.system("genfstab -U /mnt >> /mnt/etc/fstab")
-os.system("cp -r ../tali /mnt/tali")
+os.system("cp -r /root/tali /mnt/tali")
 os.system("cp /tmp/tali.ini /mnt/tmp/tali.ini")
 os.system("arch-chroot /mnt python tali/install.py")
