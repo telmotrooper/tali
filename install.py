96bf5621a8b62bd4a50756fefeaec5139ef5f646
Telmo "Trooper <telmo.trooper@gmail.com>
2019-08-29 20:23:24 -0300

    Adjustment to steps

--- install.py
+++ install.py
@@ -2,23 +2,35 @@
 
 import configparser
 
+# global variables
 path = "/tmp/tali.ini"
-
 config = configparser.ConfigParser()
 
-config.read(path)
-
-if config.read(path) == []: # no config file found
-  config["DEFAULT"] = { 'Step': '1' }
-
-  with open('/tmp/tali.ini', 'w') as config_file:
+def write_config_file():
+  with open('/tmp/tali.ini', 'w+') as config_file:
     config.write(config_file)
 
-step = int(config["DEFAULT"]["Step"])
+def main():
+  config.read(path)
 
-if step == 1:
-  import steps.step1
-elif step == 2:
-  import steps.step2
-elif step == 3:
-  import steps.step3
+  if config.read(path) == []: # no config file found
+    config["DEFAULT"] = { 'Step': '1' }
+    write_config_file()
+
+  step = int(config["DEFAULT"]["Step"])
+
+  if step == 1:
+    import steps.step1
+    config["DEFAULT"]["Step"] = '2'
+    write_config_file()
+    
+  elif step == 2:
+    import steps.step2
+    config["DEFAULT"]["Step"] = '3'
+    write_config_file()
+  
+  elif step == 3:
+    import steps.step3
+
+if __name__ == "__main__":
+  main()
