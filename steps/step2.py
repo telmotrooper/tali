fd30efd7427650c389ec122a8d56ef91b422391b
Telmo Silva <tvsilva@tech6.com.br>
2020-04-12 22:21:48 -0300

    Using yes/no dialog again

--- steps/step2.py
+++ steps/step2.py
@@ -1,11 +1,19 @@
 from func.colors import yellow
+from func.yes_no_dialog import yes_no_dialog
 
-input("Did you set your partitions, format them and mount them on " + yellow("/mnt") + "? ")
+good_to_go = yes_no_dialog("Did you set your partitions, format them and mount them on " + yellow("/mnt") + "?")
 
-import os
+if good_to_go:
+  import os
 
-os.system("pacstrap /mnt base base-devel net-tools xdg-user-dirs")
-os.system("pacstrap /mnt python wget")
-os.system("genfstab -U /mnt >> /mnt/etc/fstab")
-os.system("cp -r /root/tali /mnt/tali")
-os.system("arch-chroot /mnt python tali/install.py --step 3")
+  os.system("pacstrap /mnt base base-devel net-tools xdg-user-dirs")
+  os.system("pacstrap /mnt python wget")
+  os.system("genfstab -U /mnt >> /mnt/etc/fstab")
+  os.system("cp -r /root/tali /mnt/tali")
+  os.system("arch-chroot /mnt python tali/install.py --step 3")
+else:
+  # TODO: Make this a function, since this dialog is also used in step 1.
+  print("\nPlease, set your partitions now, format and mount them on " + yellow("/mnt") + ".")
+  print("If all you want to do is to wipe all partitions and install " + cyan("Arch Linux") + ", you can run " + green("tali/quick_partitioning.py") + ".")
+  print("Otherwise, you can format your partitions yourself with " + green("parted") + " or a similar tool.")
+  print("\nWhen you're done, run " + green("tali/install.py") + " again.\n")
