d3bed3f2204f563eb63384e4b5e38471b69b2cfa
Telmo "Trooper <telmo.trooper@gmail.com>
2021-03-18 08:37:35 -0300

    Update gen_mirrorlist.py

--- gen_mirrorlist.py
+++ gen_mirrorlist.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
 
 import os
-os.system("reflector --country Brazil --age 12 --sort rate --save /etc/pacman.d/mirrorlist")
+os.system("reflector --verbose --country Brazil --age 12 --sort rate --save /etc/pacman.d/mirrorlist")
 os.system("cat /etc/pacman.d/mirrorlist | grep Server")
