144d7ae9f0b57557d3e05f577c42c1e320e27970
Telmo "Trooper <telmo.trooper@gmail.com>
2021-09-22 18:35:26 -0300

    Fixed oh-my-zsh in post_install.py

--- steps/post_install.py
+++ steps/post_install.py
@@ -10,7 +10,7 @@ misc = "webp-pixbuf-loader" # WebP support
 os.system(f"sudo pacman -S --noconfirm {gnome_apps} ffmpegthumbnailer xorg-xkill neofetch nemo-fileroller p7zip {dvd} {misc}")
 
 print("Installing oh-my-zsh and its plugins")
-os.system("$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh) --unattended")
+os.system("sh -c \"$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh) --unattended\"")
 os.system("git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions")
 os.system("git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting")
 os.system("sed -i -e 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/g' ~/.zshrc")
