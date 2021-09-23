bc70b345b23298a9b8de112196acf46293c52b01
Telmo "Trooper <telmo.trooper@gmail.com>
2021-09-22 21:09:50 -0300

    Update dl.sh

--- dl.sh
+++ dl.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+branch=main
+
+if [ -z "$1" ]
+  then
+    echo "Default branch"
+  else
+    branch=$1
+fi
+
 printf "Generating mirrorlist with mirrors from Brazil for faster downloads...\n"
 reflector --verbose --threads 4 --protocol http,https --country Brazil --age 12 --sort rate --save /etc/pacman.d/mirrorlist
 cat /etc/pacman.d/mirrorlist | grep Server
@@ -8,7 +17,7 @@ printf "Downloading package \"git\"...\n"
 sudo pacman -Sy git --noconfirm
 
 printf "Cloning installer...\n"
-git clone https://github.com/telmotrooper/tali.git
+git clone --branch $branch https://github.com/telmotrooper/tali.git
 
 printf "Executing installer...\n"
 tali/install.py
