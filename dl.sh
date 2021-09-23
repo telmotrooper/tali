ff7a64b64b47b76dfde50ec6928cd136bf800afd
Telmo "Trooper <telmo.trooper@gmail.com>
2021-09-22 21:10:38 -0300

    Update dl.sh

--- dl.sh
+++ dl.sh
@@ -16,7 +16,7 @@ cat /etc/pacman.d/mirrorlist | grep Server
 printf "Downloading package \"git\"...\n"
 sudo pacman -Sy git --noconfirm
 
-printf "Cloning installer...\n"
+printf "Cloning installer from branch $branch...\n"
 git clone --branch $branch https://github.com/telmotrooper/tali.git
 
 printf "Executing installer...\n"
