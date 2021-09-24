75cf2804f3dee5704796e1c7fa11d76b1b97b9f0
Telmo Valverde <telmo.trooper@gmail.com>
2021-09-24 13:24:39 -0300

    Check if running Cinnamon when running post_install_user

--- steps/post_install_user.py
+++ steps/post_install_user.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import os, sys
+import subprocess
 
 print("Installing oh-my-zsh and its plugins")
 os.system("sh -c \"$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh) --unattended\"")
@@ -8,16 +9,20 @@ os.system("git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-z
 os.system("git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting")
 os.system("sed -i -e 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/g' ~/.zshrc")
 
-# Cinnamon
-os.system("gsettings set org.cinnamon.desktop.wm.preferences theme 'Arc'")     # Window borders
-os.system("gsettings set org.cinnamon.desktop.interface icon-theme 'Papirus'") # Icons
-os.system("gsettings set org.cinnamon.desktop.interface gtk-theme 'Arc'")      # Controls
-os.system("gsettings set org.cinnamon.theme name 'Arc-Dark'")                  # Desktop
-# The mouse is "org.cinnamon.desktop.interface cursor-theme"
-os.system("gsettings set org.gnome.libgnomekbd.keyboard layouts \"['br']\"")   # Set keyboard layout
+running_cinnamon = subprocess.check_output(
+  "pgrep cinnamon; exit 0;",
+  shell=True, stderr=subprocess.STDOUT).decode()
 
-# GNOME Terminal
-os.system("dconf write /org/gnome/terminal/legacy/theme-variant \"'dark'\"")
+if running_cinnamon:
+    os.system("gsettings set org.cinnamon.desktop.wm.preferences theme 'Arc'")     # Window borders
+    os.system("gsettings set org.cinnamon.desktop.interface icon-theme 'Papirus'") # Icons
+    os.system("gsettings set org.cinnamon.desktop.interface gtk-theme 'Arc'")      # Controls
+    os.system("gsettings set org.cinnamon.theme name 'Arc-Dark'")                  # Desktop
+    # The mouse is "org.cinnamon.desktop.interface cursor-theme"
+    os.system("gsettings set org.gnome.libgnomekbd.keyboard layouts \"['br']\"")   # Set keyboard layout
+
+    # GNOME Terminal
+    os.system("dconf write /org/gnome/terminal/legacy/theme-variant \"'dark'\"")
 
 # Remove script after it's been executed
 os.remove(sys.argv[0])
