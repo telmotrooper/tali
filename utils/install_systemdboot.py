772a03df857922ef8adf09e95c3dfd5f131c0ac5
Telmo "Trooper <telmo.trooper@gmail.com>
2021-09-27 22:50:36 -0300

    Added hard-coded systemd-boot setup

--- utils/install_systemdboot.py
+++ utils/install_systemdboot.py
@@ -3,4 +3,14 @@ import os
 from utils.get_firmware_interface import get_firmware_interface
 
 def install_systemdboot():
-    print("WORK IN PROGRESS")
+    os.system("bootctl install")
+    
+    os.system("""echo "title   Arch Linux (Zen)
+linux   /vmlinuz-linux-zen
+initrd  /initramfs-linux-zen.img
+options root=/dev/sda2 rw" > /boot/loader/loader.conf""")
+
+    os.system("""echo "title   Arch Linux (Zen)
+linux   /vmlinuz-linux-zen
+initrd  /initramfs-linux-zen.img
+options root=/dev/sda2 rw" > /boot/loader/entries/arch_zen.conf""")
