4e51fcff4eefae0900ab978aacc6bf5005d6a837
Telmo "Trooper <telmo.trooper@gmail.com>
2021-09-27 22:54:04 -0300

    Added TODO items

--- utils/install_systemdboot.py
+++ utils/install_systemdboot.py
@@ -5,6 +5,11 @@ from utils.get_firmware_interface import get_firmware_interface
 def install_systemdboot():
     os.system("bootctl install")
     
+    # TODO:
+    # Check which kernels are installed
+    # Find out on which drive the system was installed
+    # Find out which partition is the root partition
+
     os.system("""echo "title   Arch Linux (Zen)
 linux   /vmlinuz-linux-zen
 initrd  /initramfs-linux-zen.img
