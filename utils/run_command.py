1aee04a32f6565ba24f9236e618b9c2b8a7e4f90
Telmo "Trooper <telmo.trooper@gmail.com>
2021-09-28 19:56:56 -0300

    Revert "Testing not piping stderr to stdout"
    This reverts commit ce2d1879f5d5a774a09fd45a9861d93aedbd39ca.

--- utils/run_command.py
+++ utils/run_command.py
@@ -2,22 +2,9 @@ import subprocess, sys
 
 def run_command(command: str):
     try:
-        output = subprocess.check_output(command, shell=True).decode().rstrip()
+        output = subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT).decode().rstrip()
         return output
     except subprocess.CalledProcessError as e:
         print("An error occurred:")
         print(e.output)
         sys.exit()
-
-def run_and_print_command(command: str):
-    process = subprocess.Popen(command, stdout=subprocess.PIPE, universal_newlines=True)
-
-    for line in iter(process.stdout.readline, ""):
-        yield line
-    
-    process.stdout.close()
-
-    return_code = process.wait()
-
-    if return_code:
-        raise subprocess.CalledProcessError(return_code, command)
