03dcba5f637cf222a816846049467d781b2d3e01
Telmo "Trooper <telmo.trooper@gmail.com>
2021-09-28 19:57:00 -0300

    Revert "Step 2 now uses 'run_command' instead of 'os.system'"
    This reverts commit 2a964475abcf99f406125b14da31c68a3bc85362.

--- steps/step2.py
+++ steps/step2.py
@@ -2,7 +2,6 @@ from utils.colors import cyan, green, yellow
 from utils.select import select
 from utils.suggest_partitioning import suggest_partitioning
 from utils.yes_no_dialog import yes_no_dialog
-from utils.run_command import run_command
 
 good_to_go = yes_no_dialog("Did you set your partitions, format them and mount them on " + yellow("/mnt") + "?")
 
@@ -19,11 +18,11 @@ if good_to_go:
     ])
   )
 
-  run_command(f"pacstrap /mnt base base-devel {kernel_packages} linux-firmware net-tools xdg-user-dirs")
-  run_command("pacstrap /mnt python wget")
-  run_command("genfstab -U /mnt >> /mnt/etc/fstab")
-  run_command("cp -r /root/tali /mnt/tali")
-  run_command("cp /etc/pacman.d/mirrorlist /mnt/etc/pacman.d/mirrorlist")
-  run_command("arch-chroot /mnt python tali/install.py --step 3")
+  os.system(f"pacstrap /mnt base base-devel {kernel_packages} linux-firmware net-tools xdg-user-dirs")
+  os.system("pacstrap /mnt python wget")
+  os.system("genfstab -U /mnt >> /mnt/etc/fstab")
+  os.system("cp -r /root/tali /mnt/tali")
+  os.system("cp /etc/pacman.d/mirrorlist /mnt/etc/pacman.d/mirrorlist")
+  os.system("arch-chroot /mnt python tali/install.py --step 3")
 else:
   suggest_partitioning()
