8f389c95ad4852a3441aec7d3d3070dd2e2ee28e
Telmo "Trooper <telmo.trooper@gmail.com>
2021-09-28 18:48:48 -0300

    Added kernel listing to install_systemdboot

--- utils/install_systemdboot.py
+++ utils/install_systemdboot.py
@@ -1,10 +1,17 @@
 import os
 
 from utils.get_firmware_interface import get_firmware_interface
+from utils.run_command import run_command
+
 
 def install_systemdboot():
     os.system("bootctl install")
     
+    kernels_installed = run_command("pacman -Q | awk '/^linux/' | awk '!/headers|firmware/' | awk '{print $1}'")
+
+    print("Kernels installed are:")
+    print(kernels_installed.splitlines())
+
     # TODO:
     # Check which kernels are installed
     # Find out on which drive the system was installed
---
+++ utils/run_command.py
@@ -0,0 +1,3 @@
+def run_command(command: str):
+    output = subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT).decode().rstrip()
+    return output
