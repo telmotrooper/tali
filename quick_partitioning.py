12086dad343792dc827c59d1648cad482926f935
Telmo "Trooper <telmo.trooper@gmail.com>
2021-12-13 14:39:54 -0300

    Removed print statement

--- quick_partitioning.py
+++ quick_partitioning.py
@@ -12,9 +12,7 @@ def main():
   parser = argparse.ArgumentParser()
   parser.add_argument("--debug", action="store_true")
   args = parser.parse_args()
-
-  print(args)
-
+  
   ram = get_ram_amount()
 
   print("Which disk should be partitioned? ")
